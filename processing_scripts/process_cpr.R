3588642f4044b2ce5feadc1c4161feb179894cab
Kelly Robinson <kellylynnrobinson@gmail.com>
2017-02-20 22:01:35 -0600

    updated "process_cpr.R" 2017-02-20

--- processing_scripts/process_cpr.R
+++ processing_scripts/process_cpr.R
@@ -1,7 +1,8 @@
 
-cpr <- read.table(file = "data_sets/CPR/cpr_data.csv", sep = ",", header = TRUE, stringsAsFactors = FALSE)
+cpr <- read.table(file = "data_sets/CPR/cpr_data_20170211.csv", sep = ",", header = F, stringsAsFactors = FALSE, skip = 1)
 
-cpr <- cpr[,c("year","volume","variety","veracity","project")]
+colnames(cpr) <- c("year","volume","variety","veracity","project")
+
 cpr$volume<-as.numeric(cpr$volume)
 cpr$big.data <- cpr$volume*cpr$variety*cpr$veracity
 cpr$big.data.CumSum <- cumsum(cpr$big.data)
